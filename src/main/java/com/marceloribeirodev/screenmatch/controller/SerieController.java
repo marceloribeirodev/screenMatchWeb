947ff56458c656171b1c1de878b0598fe71da251
unknown <contatoceloribeiro@gmail.com>
2024-12-30 17:03:03 -0300

    Criação da requisição GET de Top 5 séries

--- src/main/java/com/marceloribeirodev/screenmatch/controller/SerieController.java
+++ src/main/java/com/marceloribeirodev/screenmatch/controller/SerieController.java
@@ -1,26 +1,28 @@
 package com.marceloribeirodev.screenmatch.controller;
 
 import com.marceloribeirodev.screenmatch.dto.SerieDTO;
-import com.marceloribeirodev.screenmatch.model.Serie;
-import com.marceloribeirodev.screenmatch.repository.SerieRepository;
+import com.marceloribeirodev.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
-    private SerieRepository repositorio;
+    private SerieService serieService;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obterSeries(){
-        return repositorio.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return serieService.obterTodasAsSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series(){
+        return serieService.obterTop5Series();
     }
 }
---
+++ src/main/java/com/marceloribeirodev/screenmatch/service/SerieService.java
@@ -0,0 +1,31 @@
+package com.marceloribeirodev.screenmatch.service;
+
+import com.marceloribeirodev.screenmatch.dto.SerieDTO;
+import com.marceloribeirodev.screenmatch.model.Serie;
+import com.marceloribeirodev.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository serieRepository;
+
+    private List<SerieDTO> converteDados(List<Serie> series){
+        return series.stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterTodasAsSeries(){
+        return converteDados(serieRepository.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(serieRepository.findTop5ByOrderByAvaliacaoDesc());
+    }
+}

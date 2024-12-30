48b316c230bb58c3f9190e30ed705b94d8ba1b99
unknown <contatoceloribeiro@gmail.com>
2024-12-30 15:42:48 -0300

    Criação do Controller de Serie e DTO e configuração do CORS

---
+++ src/main/java/com/marceloribeirodev/screenmatch/controller/SerieController.java
@@ -0,0 +1,27 @@
+package com.marceloribeirodev.screenmatch.controller;
+
+import com.marceloribeirodev.screenmatch.dto.SerieDTO;
+import com.marceloribeirodev.screenmatch.model.Serie;
+import com.marceloribeirodev.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class SerieController {
+
+    @Autowired
+    private SerieRepository repositorio;
+
+    @GetMapping("/series")
+    public List<SerieDTO> obterSeries(){
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/com/marceloribeirodev/screenmatch/dto/SerieDTO.java
@@ -0,0 +1,13 @@
+package com.marceloribeirodev.screenmatch.dto;
+
+import com.marceloribeirodev.screenmatch.model.Categoria;
+
+public record SerieDTO(Long id,
+                       String titulo,
+                       Integer totalTemporadas,
+                       Double avaliacao,
+                       Categoria genero,
+                       String atores,
+                       String poster,
+                       String sinopse) {
+}
